665220ddbbbd900105bdf6d3fd0c21db21459dd7
edisonivaan <edisonleo0000@gmail.com>
2022-11-19 20:40:24 -0500

    pequeño cambio en App.java

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Hello, Edison!");
+        System.out.println(Honey <3 );
     }
 }
